99ee11fb5953cd9bc02fc88e9a2d79894fb7e218
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2022-08-05 22:14:57 +0900

    [Silver V] Title: 나이순 정렬, Time: 72 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//bool ret(tuple<int, int, string > s1,tuple<int, int, string > s2) {
+//
+//	if (get<0>(s1) == get<0>(s2)) {
+//		if (get<1>(s1) > get<1>(s2))
+//			return get<1>(s1) < get<1>(s2);
+//	}
+//	else{
+//		return get<0>(s1) < get<0>(s2);
+//	}
+//}
+
+bool ret(pair<int, string> a, pair<int, string> b) {
+	return a.first < b.first;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n; cin >> n;
+	vector <pair<int, string>> ls;
+	for (int i = 0; i < n; i++) {
+		int t; string s; cin >> t >> s;
+		ls.push_back({ t,s });
+	}
+	stable_sort(ls.begin(), ls.end(), ret);
+	// cout << endl;
+	for (int i = 0; i < n; i++) {
+		cout << ls[i].first << " " << ls[i].second << '\n';
+	}
+}
