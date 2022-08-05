d1cf8b3696ea88c6f2e6f7f9030524666e396a7c
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2022-08-05 19:51:41 +0900

    [Silver V] Title: 단어 정렬, Time: 936 ms, Memory: 4532 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool customostr(string a, string b) {
+	if (a.length() == b.length()) {
+		for (int i = 0; i < a.length(); i++) {
+			if (a[i] != b[i])
+				return a[i] < b[i];
+		}
+	}
+
+	return a.length() < b.length();
+}
+
+int main() {
+	vector <string> str;
+	int a;
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		string temp; cin >> temp;
+		str.push_back(temp);
+	}
+	
+	sort(str.begin(), str.end(), customostr);
+	str.erase(unique(str.begin(), str.end()), str.end());
+
+	//sort(str.begin(), str.end());
+	for (auto& n : str) {
+		cout << n << endl;
+	}
+}
