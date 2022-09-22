501cd69da09bcdf042b9b221f9dd422253cb735b
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2022-09-23 01:28:43 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 140 ms, Memory: 32648 KB -BaekjoonHub

--- 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.cc
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.cc
@@ -1,47 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-void prt(const vector <T>& v1) {
-	cout << endl;
-	for (int i = 0; i < v1.size(); i++) {
-		for (int j = 0; j < v1[i].size(); j++) {
-			cout << v1[i][j] << "";
-		}
-		cout << endl;
-	}
-}
-
-int dx[4] = { 0,0,-1,1 };
-int dy[4] = { 1,-1,0,0 };
-
-int bfs(vector<vector<int>>& v1, vector<vector<int>>& visit, int i, int j) {
-	int nowCnt = 1;
-	queue<pair<int, int>> q;
-	pair<int, int> tmp;
-	q.push({ i,j });
-	visit[i][j] = 1;
-	while (!q.empty()) {
-		tmp = q.front(); q.pop();
-		int x = tmp.first;
-		int y = tmp.second;
-		for (int k = 0; k < 4; k++) {
-			int nX = dx[k] + x;
-			int nY = dy[k] + y;
-
-			if (nX > -1 && nY > -1 && nX < v1.size() && nY < v1[0].size()) {
-				if (visit[nX][nY] != 1 && v1[nX][nY] == 1) {
-					nowCnt++;
-					q.push({ nX, nY });
-					visit[nX][nY] = 1;
-				}
-			}
-		}
-	}
-
-	return nowCnt;
-}
-
 int start = 0, end = 0;
 
 void dfs(vector<int> graph[], int start, bool visit[]) {
