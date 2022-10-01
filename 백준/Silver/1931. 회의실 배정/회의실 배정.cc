46156aa72ba9570706da7896232c25c346e5967b
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2022-10-01 17:10:56 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { 1,-1, 0, 0 };
+
+template <typename T>
+void prt(const vector <T>& v1) {
+	cout << endl;
+	for (int i = 0; i < v1.size(); i++) {
+		for (int j = 0; j < v1[i].size(); j++) {
+			cout << v1[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+template <typename T>
+void bfs(vector <T>& mp, vector <T>& visit, int i, int j) {
+	queue <pair<int, int>> q;
+	pair <int, int> tmp;
+	q.push({ i,j });
+	visit[i][j] = 1;
+
+	while (!q.empty()) {
+		tmp = q.front(); q.pop();
+		int x = tmp.first, y = tmp.second;
+
+		for (int k = 0; k < 4; k++) {
+			int nX = x + dx[k];
+			int nY = y + dy[k];
+
+			if (nX < mp.size() && nY < mp[0].size() && nX > -1 && nY > -1) {
+				if (visit[nX][nY] == 0 && mp[nX][nY] > 0) {
+					q.push({ nX,nY });
+					visit[nX][nY] = 1;
+				}
+			}
+		}
+	}
+}
+
+template <typename T>
+void init(vector <T> &vec) {
+	for (int i = 0; i < vec.size(); i++) {
+		for (int j = 0; j < vec[0].size(); j++) {
+			vec[i][j] = 0;
+		}
+	}
+}
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n; cin >> n;
+
+	vector <pair<int, int>> vc;
+
+	int cnt = 1;
+
+	for (int i = 0; i < n; i++) {
+		int start, end; cin >> start >> end;
+		vc.push_back({ end, start});
+	}
+
+	sort(vc.begin(), vc.end());
+	int k = 1;
+	int init = vc[0].first;
+	while (k < vc.size()) {
+		if (vc[k].second >= init) {
+			cnt++;
+			init = vc[k].first;
+		}
+		k++;
+	}
+
+	cout << cnt;
+
+
+}
