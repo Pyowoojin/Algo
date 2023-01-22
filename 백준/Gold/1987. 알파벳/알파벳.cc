a687c3e9fe4663cfab9371e38cc894cc5ecdb9b1
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2023-01-22 22:21:17 +0900

    [Gold IV] Title: 알파벳, Time: 568 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -5,6 +5,8 @@ int dx[4] = { -1,0,1,0 };
 int dy[4] = { 0,1,0,-1 };
 bool alpha[26];
 
+
+// 디버깅을 위한 print 함수
 void prt(vector <vector <char>>& vc) {
 	for (int i = 0; i < vc.size(); i++) {
 		for (int j = 0; j < vc[0].size(); j++)
@@ -13,25 +15,14 @@ void prt(vector <vector <char>>& vc) {
 	}
 }
 
-void init(vector <vector <char>> &vc) {
-	/*for (int i = 0; i < 26; i++) {
-		alpha[i] = false;
-	}*/
-
-	for (int i = 0; i < vc.size(); i++) {
-		for (int j = 0; j < vc[0].size(); i++) {
-			vc[i][j] = '0';
-		}
-	}
-}
-
 // A = 17
 
-int Backtracking(const vector <vector <char>>& vc, vector <vector <char>>& visit, int i, int j) {
+int Backtracking(const vector <vector <char>>& vc, int i, int j) {
 	pair <int, int> tmp;
 	tmp.first = i;
 	tmp.second = j;
 
+	// 방문하지 않은 지역이라면, 방문 처리를 하고 gift를 1 증가시킴
 	int sum = 0;
 	if (!alpha[(vc[i][j] - '0' - 17)]) {
 		alpha[(vc[i][j] - '0' - 17)] = true;
@@ -45,11 +36,12 @@ int Backtracking(const vector <vector <char>>& vc, vector <vector <char>>& visit
 
 		if (nX > -1 && nY > -1 && nX < vc.size() && nY < vc[0].size()) {
 			if ((alpha[vc[nX][nY] - '0' - 17]) == false) {
-				k = max(Backtracking(vc, visit, nX, nY), k);
+				k = max(Backtracking(vc, nX, nY), k);
 			}
 		}
 	}
 
+	// 백트래킹을 위해 현재 방문한 지역은 방문 처리를 취소함
 	alpha[(vc[i][j] - '0' - 17)] = false;
 
 	return sum + k;
@@ -65,7 +57,6 @@ int main() {
 
 	vector <char> vc2(c, '0');
 	vector <vector <char>> vc(r, vc2);
-	vector <vector <char>> visit(vc);
 
 	// prt(vc);
 
@@ -74,12 +65,14 @@ int main() {
 		for (int j = 0; j < s.length(); j++) {
 			vc[i][j] = s[j];
 		}
-	}
+	} // 예제 입력 끝
 
-	visit[0][0] = '1';
+
+	// (0,0)은 방문 처리하고 선물 + 1
 	alpha[(vc[0][0] - '0' - 17)] = true;
 	int gift = 1;
 
-	cout << Backtracking(vc, visit, 0, 0) + gift;
+	// 백트래킹 수행 후 출력
+	cout << Backtracking(vc, 0, 0) + gift;
 
 }
