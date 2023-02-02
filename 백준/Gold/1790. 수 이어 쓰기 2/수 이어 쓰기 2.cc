b7273ebed8b4e59e876eb383c8c5a5cd3e871ea0
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2023-02-02 14:03:20 +0900

    [Gold V] Title: 수 이어 쓰기 2, Time: 1308 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.cc
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.cc
@@ -3,16 +3,14 @@ using namespace std;
 
 int WriteNum(int start, int end, int targetNum) {
 	// string에 저장없이 쭉 쓰다가 k번째 자리가 나오면 그거 출력하고 그냥 끝내기
-	int nowNum = 0;
+	int nowNumLen = 0;
 	int ans = -1;
 	while (start <= end) {
-		// 지금 더하는 숫자가 targetNum을 지나친다면 해당 숫자 추적 후 출력
 		string nowNumStr = to_string(start);
 
-		nowNum += nowNumStr.size();
-
-		if (nowNum >= targetNum) {
-			int tmp = nowNum - nowNumStr.size();
+		nowNumLen += nowNumStr.size();
+		if (nowNumLen >= targetNum) {
+			int tmp = nowNumLen - nowNumStr.size();
 			ans = nowNumStr[targetNum - tmp - 1] - '0';
 
 			break;
@@ -23,12 +21,7 @@ int WriteNum(int start, int end, int targetNum) {
 }
 
 int main() {
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios::sync_with_stdio(false);
-
 	int n, k; cin >> n >> k;
-
-	// 메모리 초과 해결 방법
+    
 	cout << WriteNum(1, n, k);
 }
