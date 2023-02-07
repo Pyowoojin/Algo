28ccd28fd94d9cd6cc912aebd0265bb0c4e36a73
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2023-02-08 02:55:43 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+// 17 - 42
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	// string s; cin >> s;
+
+	int n; cin >> n;
+
+	cout << n << endl << "1";
+
+}
