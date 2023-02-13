4769e5e9def1fe3a1c8a602544e2c698a70b05d0
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2023-02-13 13:39:44 +0900

    [Silver III] Title: N과 M (7), Time: 492 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector <int> ans;
+
+void dfs(int k, int maxN, vector <int>& num, vector <int>& visited, int nowNum) {
+	if (k == maxN) {
+		for (auto i : ans)
+			cout << i << " ";
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < num.size(); i++) {
+		ans.push_back(num[i]);
+		dfs(k + 1, maxN, num, visited, num[i]);
+		ans.pop_back();
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, m; cin >> n >> m;
+
+	vector <int> num;
+
+	for (int i = 0; i < n; i++) {
+		int c; cin >> c;
+		num.push_back(c);
+	}
+
+	sort(num.begin(), num.end());
+	vector <int> visited(n);
+
+	dfs(0, m, num, visited, 0);
+}
