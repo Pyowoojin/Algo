8164f24a6de32e19cf3ee316f6d73165964e13e1
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2023-02-15 17:54:49 +0900

    [Gold V] Title: 최소비용 구하기, Time: 40 ms, Memory: 4748 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dijkstra(vector <vector <pair <int, int>>>& graph, int start, vector <int> & dist) {
+	dist[start] = 0;
+	priority_queue <pair <int, int>> pq;
+
+	pq.push({ 0, start });
+
+	while (!pq.empty()) {
+		int cost = -pq.top().first;
+		int cur = pq.top().second;
+		pq.pop();
+
+		if (dist[cur] < cost) continue;
+
+		for (int i = 0; i < graph[cur].size(); i++) {
+			int next = graph[cur][i].first;
+			int nCost = graph[cur][i].second;
+
+			if (dist[next] > nCost + cost) {
+				dist[next] = cost + nCost;
+				pq.push({ -dist[next], next });
+				continue;
+			}
+		}
+	}
+
+}
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, m; cin >> n >> m;
+
+	vector < vector <pair <int, int>>> graph(n);
+	vector <int> dist(n);
+	fill(dist.begin(), dist.end(), 0x7fffffff);
+
+	for (int i = 0; i < m; i++) {
+		int from, to, cost; cin >> from >> to >> cost;
+		graph[from-1].push_back({ to-1, cost });
+		// graph[to-1].push_back({ from -1 ,cost });
+	}
+
+	int start, end; cin >> start >> end;
+
+	dijkstra(graph, start-1, dist);
+
+	cout << dist[end - 1];
+}
