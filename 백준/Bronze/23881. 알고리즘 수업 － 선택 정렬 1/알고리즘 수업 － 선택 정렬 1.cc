0f7ca3f595d6476800a99ec88d18b4e4c66bf424
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2023-02-17 16:26:37 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 160 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int totalCount = 0;
+vector <int> ans;
+
+void swapp(int arr[], int a, int b, int k) {
+	totalCount++;
+
+	if (totalCount == k) {
+		ans.push_back(arr[a]);
+		ans.push_back(arr[b]);
+	}
+
+	// cout << arr[a] << ", " << arr[b] << endl;
+
+	int tmp = arr[b];
+	arr[b] = arr[a];
+	arr[a] = tmp;
+}
+
+void selection_sort(int arr[], int arrSize, int k) {
+	for (int i = arrSize - 1; i > 0; i--) {
+		int maxN = i;
+		for (int j = i - 1; j > -1; j--) {
+			if (arr[maxN] < arr[j])
+				maxN = j;
+		}
+		// cout << i << " 번 loop의 maxN : " << maxN << endl;
+		if (maxN != i) {
+			swapp(arr, maxN, i, k);
+		}
+		// cout << i << "번 정렬 : ";
+		
+		/*for (auto i = 0; i < arrSize; i++)
+			cout << arr[i] << " ";
+		cout << endl;*/
+	}
+
+}
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(0);
+
+	int arrSize, swapCount;
+	cin >> arrSize >> swapCount;
+
+	int *arr = new int[arrSize];
+
+	for (int i = 0; i < arrSize; i++) {
+		int c; cin >> c;
+		arr[i] = c;
+	}
+
+	selection_sort(arr, arrSize, swapCount);
+
+	if (ans.size() != 0) {
+		sort(ans.begin(), ans.end());
+		for (auto i : ans)
+			cout << i << " ";
+	}
+	else
+		cout << "-1";
+}
