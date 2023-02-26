7d52fb7b6712daa8733693fb8414358c7b881973
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2023-02-26 22:27:32 +0900

    [Bronze II] Title: A+B - 6, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		string s; cin >> s;
+		cout << (s[0] - '0') + (s[2] - '0') << endl;
+	}
+}
