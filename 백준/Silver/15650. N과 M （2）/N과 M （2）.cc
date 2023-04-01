4d87de4f700c196693a48e7e8c682bcad5e7dc1f
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2023-04-01 12:33:10 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,50 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-vector <int> vc;
-
-void prt(vector <int>& vc) {
-	for (int i = 0; i < vc.size(); i++) {
-		if (i > 0 && vc[i - 1] > vc[i])
-			return;
-	}
-
-	for (int i = 0; i < vc.size(); i++) {
-		cout << vc[i] << " ";
-	}
-	cout << '\n';
-}
-
-void dfs(int k, int visited[]) {
-	if (vc.size() == m) {
-		prt(vc);
+void Backtracking(vector <int> &list, vector <int> &ans, int cnt, int maxX, int nowI) {
+	if (cnt == maxX) {
+		for (int i = 0; i < ans.size(); i++) {
+			cout << ans[i] << " ";
+		}
+		cout << endl;
 		return;
 	}
 
-	for (int i = 0; i < n; i++) {
-		// 이미 방문한 배열이면 다음 배열로 넘어감
-		if (visited[i] == 1)
-			continue;
-		// 그렇지 않다면 방문처리 후 vector에 삽입
-		visited[i] = 1;
-		vc.push_back(i + 1);
-		// 재귀 수행
-		dfs(i + 1, visited);
-		vc.pop_back();
-		visited[i] = 0;
+	for (int i = 0; i < list.size(); i++) {
+		if (i >= nowI) {
+			ans.push_back(list[i]);
+			Backtracking(list, ans, cnt + 1, maxX, list[i]);
+			ans.pop_back();
+		}
 	}
-
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	
+	int n, m; cin >> n >> m;
+	vector <int> list;
+	vector <int> ans;
+	for (int i = 1; i <= n; i++) {
+		list.push_back(i);
+	}
 
-	cin >> n >> m;
+	Backtracking(list, ans, 0, m, 0);
 
-	int* visited = new int[n];
-	fill(visited, visited + n, 0);
-	dfs(0, visited);
 }
