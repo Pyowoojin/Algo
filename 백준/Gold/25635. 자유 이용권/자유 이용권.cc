b0027997109dabea3b365374e054c3ca1053374b
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2023-04-09 13:27:42 +0900

    [Gold IV] Title: 자유 이용권, Time: 16 ms, Memory: 3684 KB -BaekjoonHub

--- 백준/Gold/25635. 자유 이용권/자유 이용권.cc
+++ 백준/Gold/25635. 자유 이용권/자유 이용권.cc
@@ -1,17 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
-void prt(const vector <T>& v1) {
-	cout << endl;
-	for (int i = 0; i < v1.size(); i++) {
-		for (int j = 0; j < v1[i].size(); j++) {
-			cout << v1[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
-
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
