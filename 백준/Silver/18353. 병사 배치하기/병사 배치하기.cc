05e1c683d42d32882dba8e861dc63080f860cb02
Wooooojin <66067291+Pyowoojin@users.noreply.github.com>
2023-11-21 15:15:58 +0900

    [Silver II] Title: 병사 배치하기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	vector <int> vc;
+	vector <int> dp(n);
+
+	for (int i = 0; i < n; i++) {
+		int c; cin >> c;
+		vc.push_back(c);
+	}
+
+	dp[0] = 1;
+	int ans = 1;
+
+	for (int i = 1; i < n; i++) {
+		dp[i] = 1;
+
+		for (int j = 0; j < i; j++) {
+			if (vc[j] > vc[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+				ans = max(ans, dp[i]);
+			}
+		}
+	}
+
+	cout << n - ans;
+}
